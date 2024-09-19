da7dc92a7a6d7753dec45c9131c684bf0282861b
Bruce-Ngobeni <brucengobeni2005@gmail.com>
2024-09-19 13:04:32 +0200

    Added movie route to g route using get method, to fetch data from omdb using omdbapi and send the response to the client

--- app.mjs
+++ app.mjs
@@ -4,6 +4,19 @@ import fetch from "node-fetch";
 const PORT = 3000;
 const app = express();
 
+const url = "https://www.omdbapi.com/?t=Power+Book+II&apikey=b9b186b5"
+
+app.get("/movie", (req, res) => {
+
+    fetch(url)
+    .then((data) => {
+        return data.json();
+    })
+    .then((data) => {
+        res.send(data);
+    })
+})
+
 
 app.listen(PORT, () => {
     console.log("MoviewDB server is up and running on port ", PORT);
